6102316093424924c14504b88d406f81515045a9
jesspring <jesswale94@gmail.com>
2025-02-06 02:41:34 +0000

    Fix error when loading images

--- src/scripts/images.ts
+++ src/scripts/images.ts
@@ -5,7 +5,7 @@ export async function getImage(imageName: string) {
 
     const img = document.createElement("img");
     loadedImages[imageName] = img;
-    img.src = "/images/" + imageName + ".png";
+    img.src = "images/" + imageName + ".png";
     document.body.appendChild(img);
     img.style.cssText = "display: none";
     await new Promise(resolve => { img.onload = resolve; });
